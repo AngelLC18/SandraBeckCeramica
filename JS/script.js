693eab2ce652a18276ef4f6c690c60b454eaa34c
Angel Luciano Ciancio <ciancioangel16@gmail.com>
2022-11-12 17:13:18 -0300

    Agregando evento para volver al menu desplegable fijo dentro de la pagina web

--- JS/script.js
+++ JS/script.js
@@ -2,3 +2,8 @@ window.addEventListener("scroll", function() {
     var header = document.querySelector("header");
     header.classList.toggle('down', window.scrollY > 0);
 });
+
+window.addEventListener("scroll", function() {
+    let navmenu__menu = document.querySelector(".nav-menu__menu");
+    navmenu__menu.classList.toggle('down', window.scrollY > 0);
+});
