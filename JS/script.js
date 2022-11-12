44e0f456c07261fc9555ff16c4d4a6701c7aef93
Angel Luciano Ciancio <ciancioangel16@gmail.com>
2022-11-12 17:13:45 -0300

    Agregando evento para crear un boton que scrollee hacia arriba la pagina web

--- JS/script.js
+++ JS/script.js
@@ -7,3 +7,12 @@ window.addEventListener("scroll", function() {
     let navmenu__menu = document.querySelector(".nav-menu__menu");
     navmenu__menu.classList.toggle('down', window.scrollY > 0);
 });
+
+window.addEventListener("scroll", () => {
+    const toTop = document.querySelector(".to-top");
+    if (window.pageYOffset > 100) {
+        toTop.classList.add("active");
+    } else {
+        toTop.classList.remove("active");
+    }
+})
